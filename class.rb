8ec52514f39cb8aebc0d8e1cb4e5285ad3de59c4
Bola Muyis <bolamuyis@gmail.com>
2019-01-16 04:21:53 -0500

    added class.rb which also contains and initiallizer

---
+++ class.rb
@@ -0,0 +1,29 @@
+class Cake
+    attr_accessor :name, :price, :description, :picture
+    def initialize(name,price,description,picture)
+        @name = name
+        @price = price
+        @description = description
+        @picture = picture
+    end
+end
+
+class Cookies
+    attr_accessor :name, :price, :description, :picture
+    def initialize(name,price,description,picture)
+        @name = name
+        @price = price
+        @description = description
+        @picture = picture
+    end
+end
+
+class Muffins
+    attr_accessor :name, :price, :description, :picture
+    def initialize(name,price,description,picture)
+        @name = name
+        @price = price
+        @description = description
+        @picture = picture
+    end
+end
